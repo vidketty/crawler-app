8a700aa14aff2109039f98324313d8c86d8a765f
vidketty <vidketty@gmail.com>
2019-10-02 01:34:14 +0530

    - Added routes for do - crawling

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var Crawler = require("crawler");
+const express = require('express')
+const app = express()
+const Crawler = require("crawler");
+let $;
+// = require("jquery");
 
-var c = new Crawler({
+const c = new Crawler({
 	maxConnections: 10,
 	// This will be called for each crawled page
 	callback: function (error, res, done) {
@@ -10,30 +14,74 @@ var c = new Crawler({
 			var $ = res.$;
 			// $ is Cheerio by default
 			//a lean implementation of core jQuery designed specifically for the server
-			console.log('$("title").text()', $("title").text());
+			console.log($("title").text());
 		}
 		done();
 	}
 });
 
-
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-	uri: 'https://www.chevron.com/',
-
-	// @TODO if jQuery not required. 
-	// jQuery: false,
-
-	// The global callback won't be called
-	callback: function (error, res, done) {
-		if (error) {
-			console.log(error);
-		} else {
-			// $ you can $ for jQuery.
-			const $ = res.$
-
-			console.log(res.body);
+let cache = []
+const JSONStringifyCallback = (key, value) => {
+	if (typeof value === 'object' && value !== null) {
+		if (cache.indexOf(value) !== -1) {
+			// Duplicate reference found, discard key
+			return;
 		}
-		done();
+		// Store value in our collection
+		cache.push(value);
 	}
-}]);
+	return value;
+}
+
+const doCrawling = (req, res) => {
+	c.queue([{
+		uri: 'https://www.npmjs.com/package/express',
+
+		// The global callback won't be called
+		callback: function (error, response, done) {
+			$ = response.$;
+			if (error) {
+				console.log(error);
+			} else {
+
+				getPageLinks(response.body, "a", /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/, async (links, parsedPages) => {
+					const allLinks = [...JSON.parse(JSON.stringify(links, JSONStringifyCallback))];
+					const allParsedPages = [...JSON.parse(JSON.stringify(parsedPages, JSONStringifyCallback))]
+					const allLinksArray = Object.keys(allLinks).filter((tag, index) => index)
+					for (let idx = 0; idx < 10; i++) {
+
+					}
+					res.send({
+						links: allLinks,
+						parsedPages: allParsedPages
+					})
+				})
+			}
+			done();
+		}
+	}]);
+}
+
+
+const getPageLinks = async (data, selector, regex, callback) => {
+	let parsedPages = [];
+	let url = '';
+	var links = await $(selector, data)				// jquery context is our response data
+		.filter(function () {
+			return this.href.match(regex);      // without an href matching our regex
+		})
+
+	if (url.lastIndexOf('?') != -1) {
+		url = url.substr(0, url.lastIndexOf('?')); // some weird querystring gets added i don't want
+	}
+
+	parsedPages.push(url);  			// watchin' our tracks
+	callback(links, parsedPages);
+}
+
+app.get('/', (req, res) => { res.send('Hello World') })
+
+app.get('/do-crawling', doCrawling)
+
+
+app.listen(3000)
